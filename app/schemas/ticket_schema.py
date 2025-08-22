c43f9f98df7101656a1706192ac9fa82cd6c461b
itachi <itachi_ashes@163.com>
2025-08-22 10:24:28 +0800

    修复由于schema字段确实导致查询工单详情报错的问题

--- app/schemas/ticket_schema.py
+++ app/schemas/ticket_schema.py
@@ -76,6 +76,7 @@ class TicketResponse(SQLModel):
     handling_method: Optional[str]
     handler: Optional[str]
     user_id: int
+    status: int
     create_at: datetime
     attachments: List[AttachmentOut] = []
 
